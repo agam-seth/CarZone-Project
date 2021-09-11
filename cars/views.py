07cfa75462cccf06d3f91c9fe4b5e1559842fb02
Agam Seth <agamseth1999@gmail.com>
2021-09-11 12:48:35 +0530

    completed search and admin customization

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from cars.models import Car
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 
 def cars(request):
-    return render(request, 'cars/cars.html')
+    cars = Car.objects.order_by('-created_date').all()
+
+    paginator = Paginator(cars, 4)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
+
+    data = {
+        'cars': paged_cars,
+    }
+    return render(request, 'cars/cars.html', data)
+
+def car_detail(request, id):
+    single_car = get_object_or_404(Car, pk=id)
+    data = {
+        'single_car': single_car,
+    }
+    return render(request, 'cars/car_detail.html', data)
+
+def search(request):
+    all_cars = Car.objects.order_by('-created_date').all()
+    cars = Car.objects.order_by('-created_date').all()
+
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            cars = cars.filter(description__icontains=keyword)
+
+    if 'color' in request.GET:
+        color = request.GET['color']
+        if color:
+            cars = cars.filter(color__iexact=color)
+
+    if 'model' in request.GET:
+        model = request.GET['model']
+        if model:
+            cars = cars.filter(model__iexact=model)
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            cars = cars.filter(city__iexact=city)
+
+    if 'year' in request.GET:
+        year = request.GET['year']
+        if year:
+            cars = cars.filter(year__iexact=year)
+
+    if 'body_style' in request.GET:
+        body_style = request.GET['body_style']
+        if body_style:
+            cars = cars.filter(body_style__iexact=body_style)
+
+    if 'min_price' in request.GET and 'max_price' in request.GET:
+        min_price = request.GET['min_price']
+        max_price = request.GET['max_price']
+        if max_price:
+            cars = cars.filter(price__range=(min_price, max_price))
+
+
+    data = {
+        'cars': cars,
+        'all_cars': all_cars,
+    }
+    return render(request, 'cars/search.html', data)
